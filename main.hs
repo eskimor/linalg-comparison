5c37a154f5c78f86ac3daff1dfa83e05553dd3b7
Robert Klotzner <robert.klotzner@gmx.at>
2015-03-08 01:01:12 +0100

    Benchmark 2
    We beat C++ :-)

--- main.hs
+++ main.hs
@@ -9,6 +9,7 @@ import Foreign.C.Types
 import qualified Data.Vector as V
 import Control.Applicative
 import Control.Parallel.Strategies
+import Control.Monad (forever)
 
 foreign import ccall unsafe "cppAdditionImpl" cppAdditionImpl :: CInt -> IO ()
 
@@ -67,6 +68,8 @@ vectorAdditionPar size =
                              <*> (rseq $ V.zipWith (+) part2 part2)
     in
         result1 V.++ result2
+
+
 --
 dataPoints !n = n : dataPoints (n*10)
 
@@ -74,22 +77,23 @@ dataPoints !n = n : dataPoints (n*10)
 --
 
 benchGroup size = bgroup (show size) [
-                   bench "Haskell HMatrix" $ nf hMatrixAddition size
-                   , bench "Haskell Vector seq" $  nf vectorAddition size
-                   , bench "Haskell Vector par" $  whnf vectorAdditionPar size
-                   , bench "Haskell Repa seq" $ whnf sRepaAddition size
-                   , bench "Haskell Repa par" $  whnfIO $ pRepaAddition size
+                   --bench "Haskell HMatrix" $ nf hMatrixAddition size
+                   bench "Haskell Vector seq" $  nf vectorAddition size
+                   --, bench "Haskell Vector par" $  whnf vectorAdditionPar size
+                   --, bench "Haskell Repa seq" $ whnf sRepaAddition size
+                   --, bench "Haskell Repa par" $  whnfIO $ pRepaAddition size
                    , bench "C++ ViennaCL" $  whnfIO $ cppAddition size
                   ]
            
 
 main :: IO ()
 main = defaultMain . map benchGroup . take 5 . dataPoints $ 100
-
 {--
 main = do
-  print $ vectorAddition 10
+  --forever $ print $ (vectorAddition 10000000) V.! 9999999
+  forever $ cppAddition 10000000
   print $ sRepaAddition 10
   pRepaAddition 10 >>= print
---}
   
+
+--}
